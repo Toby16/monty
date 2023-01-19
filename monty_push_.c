0ba04b7cb0f044bdeba899790096f9744b70d085
Toby16 <bolajibaasit14@gmail.com>
2023-01-19 06:48:17 -0500

    fixed betty error

--- monty_push_.c
+++ monty_push_.c
@@ -1,5 +1,7 @@
 #include "monty.h"
 
+void monty_push(stack_t **stack, unsigned int line_number);
+
 /**
  * monty_push - Pushes a value to a stack_t linked list
  * @stack: A pointer to the top mode node of a stack_t linked list
